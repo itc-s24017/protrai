db66054e1d32e0631fdc6994aec94427ab3a6c3f
itc-s24017 <s24017@std.it-college.ac.jp>
2024-06-17 06:43:28 +0900

    def4_kadai.py

---
+++ def4_kadai.py
@@ -0,0 +1,13 @@
+#24017
+#kadai
+
+import random
+def omikuji():
+    kuji = ["大吉","中吉","小吉","凶"]
+    return random.choice(kuji)
+
+def main():
+    kekka = omikuji()
+    print("結果は", kekka , "です")
+if __name__ == "__main__":
+    main()
