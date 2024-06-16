ee419c5f2804c6b1f8b09a8d74b3d99219a6a250
itc-s24017 <s24017@std.it-college.ac.jp>
2024-06-17 05:11:11 +0900

    def1.py

---
+++ def1.py
@@ -0,0 +1,9 @@
+#24017　def1.py
+#　関数で「こんにちは」を3回表示するプログラム
+
+def seyhello():
+    print("こんにちは")
+
+seyhello()
+seyhello()
+seyhello()
---
+++ for.py
@@ -0,0 +1,5 @@
+#s24017
+#for文の課題
+
+p = "s"
+for i in range(24001, 24031): print(f"{p}{i}")
